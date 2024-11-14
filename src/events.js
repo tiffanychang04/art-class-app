1d51f9015f48b16c2486dfea51abd9b700f6c289
suzannawang <suzanna.wang2002@gmail.com>
2024-11-14 00:49:29 -0500

    added images to event

--- src/events.js
+++ src/events.js
@@ -5,7 +5,8 @@ export const items = [
       "start_datetime": "2024-10-29T15:00:00-04:00",
       "duration": 2,
       "category": "Sculpture",
-      "location": "4917 Pine Street"
+      "location": "4917 Pine Street",
+      backgroundImage: "https://crafty-clayworks.com/cdn/shop/articles/text-to-image_8413ca9e-8c51-4cb8-839e-478b81c055ea.png?v=1710431363"
     },
     {
       "name": "Watercolor",
@@ -13,7 +14,8 @@ export const items = [
       "start_datetime": "2024-11-19T11:00:00-04:00",
       "duration": 1.5,
       "category": "Painting",
-      "location": "4010 Spruce Street"
+      "location": "4010 Spruce Street",
+      backgroundImage: "https://i.ytimg.com/vi/stpNWTzvXYs/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBKHMqL-Ymb_jeVghalb5N02W0i0g"
     },
     {
       "name": "Still Life",
@@ -21,7 +23,8 @@ export const items = [
       "start_datetime": "2024-11-05T17:00:00-04:00",
       "duration": 1,
       "category": "Painting",
-      "location": "Dilworth Park"
+      "location": "Dilworth Park",
+      backgroundImage: "https://zenfolio.com/wp-content/uploads/2022/10/still-life-ideas-lighting.jpg"
     },
     {
       "name": "Acrylic Pouring",
@@ -29,7 +32,8 @@ export const items = [
       "start_datetime": "2024-11-10T14:00:00-04:00",
       "duration": 3,
       "category": "Painting",
-      "location": "238 Chestnut Street"
+      "location": "238 Chestnut Street",
+      backgroundImage: "https://www.artsupplies.co.uk/blog/wp-content/uploads/2020/09/acrylic-pour-art.jpg"
     },
     {
       "name": "Drawing 101",
@@ -37,7 +41,8 @@ export const items = [
       "start_datetime": "2024-11-12T09:00:00-04:00",
       "duration": 2.5,
       "category": "Drawing",
-      "location": "Loomis Hall"
+      "location": "Loomis Hall",
+      backgroundImage: "https://mac-birmingham.transforms.svdcdn.com/production/Learning-and-Participation/2024/Courses/VA112-_-Anatomical-drawing.png?w=760&h=504&q=100&auto=format&fit=crop&dm=1716987126&s=200b044b37ee2fddb3972d081c4036e6"
     },
     {
       "name": "Sculpture Basics",
@@ -45,7 +50,8 @@ export const items = [
       "start_datetime": "2024-11-20T16:00:00-04:00",
       "duration": 3,
       "category": "Sculpture",
-      "location": "4905 Walnut Street"
+      "location": "4905 Walnut Street",
+      backgroundImage: "https://pangeasculptures.com/cdn/shop/articles/how-to-make-a-sculpture-step-by-step-822195.jpg?v=1697475490&width=2048"
     },
     {
       "name": "Abstract Painting",
@@ -53,7 +59,8 @@ export const items = [
       "start_datetime": "2024-11-25T18:00:00-04:00",
       "duration": 2,
       "category": "Painting",
-      "location": "The Art Studio"
+      "location": "The Art Studio",
+      backgroundImage: "https://ik.imagekit.io/theartling/prod/tr:w-1650/products/Product/b89d71c2d06f44de90f93b1a448bfee5_sw-3502_sh-2720.jpg"
     },
     {
       "name": "Photography Basics",
@@ -61,7 +68,8 @@ export const items = [
       "start_datetime": "2024-12-02T10:00:00-04:00",
       "duration": 4,
       "category": "Photography",
-      "location": "1125 Race Street"
+      "location": "1125 Race Street",
+      backgroundImage: "https://cdn-nyt-prd.nytlicensing.com/media/images/photojournalism-thumb.jpg"
     },
     {
       "name": "Ceramics for Beginners",
@@ -69,7 +77,8 @@ export const items = [
       "start_datetime": "2024-12-05T13:00:00-04:00",
       "duration": 2.5,
       "category": "Sculpture",
-      "location": "3124 Pine Avenue"
+      "location": "3124 Pine Avenue",
+      backgroundImage: "https://media.cca.edu/images/Ceramics_x_Game_Arts-SP23-2024_06-jrl.min-1040x585.jpg"
     }
   ];
   
